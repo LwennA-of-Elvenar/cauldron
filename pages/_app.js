7d6f488823e6c019ccef20c85cb70994c34a2989
LwennA-of-Elvenar <lwenna.mailbox@gmail.com>
2023-03-20 23:42:11 +0100

    Enable global css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-//import '@/styles/globals.css'
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   const [worker, setWorker] = useState(null);
